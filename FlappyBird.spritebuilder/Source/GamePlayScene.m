6ac46b657cf8f0b46470dab3ae4e7327466b3d06
Pavan Ravipati <psravipati@gmail.com>
2015-01-02 23:30:30 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,24 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    // increment time since last obstacle
+    timeSinceObstacle += delta;
+    
+    // check to see if 2 seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // create a new obstacle
+        [self addObstacle];
+        
+        //reset the timer to zero
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
